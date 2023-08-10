7735945cfbbc5a5d12d7b63764005071946cb937
Yooonlee <98630824+Yooonlee@users.noreply.github.com>
2023-08-10 13:55:39 +0900

    feat: urlname updated
    urlname을 drf convention에 맞게 약간 수정

--- dj_pr/uploader/urls.py
+++ dj_pr/uploader/urls.py
@@ -14,18 +14,18 @@ from .import views
 # ]
 urlpatterns = [
     # 형식 : collection이름/메서드/pk/추가정보
-    path('diagnosis/post/detail', views.DiagnosisCreateAPIView.as_view(), name='diagnosis-detail'),
+    path('diagnosis/image-upload', views.DiagnosisCreateAPIView.as_view(), name='diagnosis-image_upload'),
     # path('diagnosis/get/<int:pet_id>/list', views.DiagnosisListAPIView.as_view(), name='diagnosis-list'),
     # path('user/post/', views.UserCreateAPIView.as_view(), name='user-list'),
     # path('user/get/<int:user_id>', views.UserListAPIView.as_view(), name='user-detail'),
 
-    path('user/post/', views.UserCreateView.as_view(), name='user-create'),    
-    path('user/delete/<int:user_id>/', views.UserDestroyView.as_view(), name='user-delete'),
-    path('diagnosis/post/', views.DiagnosisCreateView.as_view(), name='diagnosis-create'),    
-    path('pet/post/', views.PetCreateView.as_view(), name='pet-create'),
-    path('pet/get/<int:pet_id>', views.PetRetrieveView.as_view(), name='pet-retrieve'),
-    path('pet/get/', views.PetListView.as_view(), name='pet-list'),
-    path('pet/delete/<int:pet_id>/', views.PetDestroyView.as_view(), name='pet-delete'),
-    path('pet/update/', views.PetUpdateView.as_view(), name='pet-update'),
+    path('user/create-user/', views.UserCreateView.as_view(), name='user-create_user'),    
+    path('user/<int:user_id>/delete-user', views.UserDestroyView.as_view(), name='user-delete_user'),
+    path('diagnosis/upload-diagnosis/', views.DiagnosisCreateView.as_view(), name='diagnosis-upload_diagnosis'),    
+    path('pet/create-pet', views.PetCreateView.as_view(), name='pet-create_pet'),
+    path('pet/<int:pet_id>/list-pet', views.PetRetrieveView.as_view(), name='pet-list_pet'),
+    path('pet', views.PetListView.as_view(), name='pet-list_all'),
+    path('pet/<int:pet_id>/delete-pet', views.PetDestroyView.as_view(), name='pet-delete_pet'),
+    path('pet/update-pet/', views.PetUpdateView.as_view(), name='pet-update_pet'),
 
 ]
